6f555db1cb303081cc19914287a36f547e89f999
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2026-03-12 12:43:50 +0700

    Menambahkan penanganan error saat koneksi database

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -43,6 +43,11 @@ namespace PraktikumADO
 
                 conn.Close();
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
         }
     }
 }
