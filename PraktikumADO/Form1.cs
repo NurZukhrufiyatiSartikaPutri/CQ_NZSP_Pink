cb4471fd6c958c5a513c0f973d76329b61a160e1
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2026-03-12 13:39:38 +0700

    Menambahkan penanganan error menggunakan try-catch

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -90,6 +90,10 @@ namespace PraktikumADO
 
                 conn.Close();
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
             
         }
     }
