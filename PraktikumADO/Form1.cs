2bbe075474445f45a818c215f39df01e1bd997d8
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2026-03-12 11:53:26 +0700

    menambahkan variabel koneksi di dalam class Form1

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -13,6 +13,8 @@ namespace PraktikumADO
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn;
+        SqlCommand cd;
         public Form1()
         {
             InitializeComponent();
